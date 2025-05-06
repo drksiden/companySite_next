f3cc50a75c4afefcfef5787bc2719ec4781b8ffe
Danil <boss.danil.kosenko.g@gmail.com>
2025-05-06 16:00:45 +0500

    update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,8 +4,6 @@ import Medusa from "@medusajs/medusa-js";
 
 const medusa = new Medusa({ baseUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000", maxRetries: 3, publishableApiKey: process.env.NEXT_PUBLIC_MEDUSA_API_KEY || '' });
 
-console.log("MEDUSA KEY:", process.env.NEXT_PUBLIC_MEDUSA_API_KEY);
-
 export const authOptions: AuthOptions = {
   providers: [
     CredentialsProvider({
